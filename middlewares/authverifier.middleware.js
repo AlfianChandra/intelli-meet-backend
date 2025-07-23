e32e0df455fbb9e5b0c794eb119d1595914a9d3e
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-07-23 15:44:54 +0700

    added allowedOrigin for prod

--- middlewares/authverifier.middleware.js
+++ middlewares/authverifier.middleware.js
@@ -10,6 +10,7 @@ const routerExclusions = [
 ]
 const allowedOrigins = [
 	'http://localhost:5173', //Dev - Vite
+	'https://intellimeet.rndkito.com',
 	'http://localhost:2512',
 	'https://accounts.google.com/', //Google
 	'PostmanRuntime', //Dev - Postman
